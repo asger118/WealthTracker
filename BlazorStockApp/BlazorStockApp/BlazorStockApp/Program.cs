1735ae5459bd36b864036d1245a0a4570695da78
AsgerVeng <Asgerveng1@gmail.com>
2025-03-03 14:37:44 +0100

    Big update boys

--- BlazorStockApp/BlazorStockApp/BlazorStockApp/Program.cs
+++ BlazorStockApp/BlazorStockApp/BlazorStockApp/Program.cs
@@ -58,3 +58,8 @@ app.MapRazorComponents<App>()
     .AddAdditionalAssemblies(typeof(BlazorStockApp.Client._Imports).Assembly);
 
 app.Run();
+
+// Create migration
+// dotnet ef migrations add 0001-initial --project BlazorStockApp.Data --startup-project BlazorStockApp/BlazorStockApp
+// Update database with migration
+// dotnet ef database update --project BlazorStockApp.Data --startup-project BlazorStockApp/BlazorStockApp
